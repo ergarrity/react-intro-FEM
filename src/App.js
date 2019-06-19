a048e02fc6fd726be658880224e9c320b631e07a
Liz Garrity <ergarrity@gmail.com>
2019-06-19 15:22:15 -0700

    Define Pet component and call within App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Callie"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2", {}, "Walldog")
+    ]);
+}
+
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "Adopt Me!")
-    );
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet)
+    ]);
 };
 
 ReactDOM.render(
