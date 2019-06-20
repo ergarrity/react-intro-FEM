2f0d01c22addf20d934ebb609ba668edd6bed763
Liz Garrity <ergarrity@gmail.com>
2019-06-20 09:13:43 -0700

    Convert components to JSX and reconfigure eslint for JSX

--- src/App.js
+++ src/App.js
@@ -3,24 +3,13 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Adopt Me!'),
-		React.createElement(Pet, {
-			name: 'Callie',
-			animal: 'Dog',
-			breed: 'Walldog'
-		}),
-		React.createElement(Pet, {
-			name: 'Mona',
-			animal: 'Dog',
-			breed: 'Princess'
-		}),
-		React.createElement(Pet, {
-			name: 'Gus',
-			animal: 'Dog',
-			breed: 'Pitbull'
-		})
-	]);
+    return (
+        <div>
+            <Pet name="Callie" animal="Dog" breed="Walldog" />
+            <Pet name="Mona" animal="Dog" breed="Princess" />
+            <Pet name="Charlie" animal="Cat" breed="Old" />
+        </div>
+    )
 };
 
 render(React.createElement(App), document.getElementById('root'));
