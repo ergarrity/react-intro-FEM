cc922f2703ed3705e557085b524ef55c8ab2c59f
Liz Garrity <ergarrity@gmail.com>
2019-06-19 19:31:54 -0700

    Add format to npm scripts to run prettier on all js and html files in src directory

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-const Pet = ({ name, animal, breed}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
-}
-
-const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, { 
-            name: "Callie", 
-            animal: "Dog", 
-            breed: "Walldog" 
-        }),
-        React.createElement(Pet, { 
-            name: "Mona", 
-            animal: "Dog", 
-            breed: "Princess" 
-        }),
-        React.createElement(Pet, { 
-            name: "Gus", 
-            animal: "Dog", 
-            breed: "Pitbull" 
-        })
-    ]);
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Callie",
+      animal: "Dog",
+      breed: "Walldog"
+    }),
+    React.createElement(Pet, {
+      name: "Mona",
+      animal: "Dog",
+      breed: "Princess"
+    }),
+    React.createElement(Pet, {
+      name: "Gus",
+      animal: "Dog",
+      breed: "Pitbull"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
